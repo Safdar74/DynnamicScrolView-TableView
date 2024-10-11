d93b233ab3708322b37621cda41e3974f866b5df
SafdarDemo <zillowbhai@gmail.com>
2024-10-11 14:47:16 +0500

    Initial Commit

---
+++ DynnamicScrolView&TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynnamicScrolView&TableView
+//
+//  Created by Safdar Rajpoot  on 11/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
